bb7e2333784a2589770f1631e0b74762d6405ef3
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-12-08 14:43:12 +0900

    feat: Terraform 기본 main.tf 구성 추가
    - component, discord-bot 설정은 주석 처리

---
+++ main.tf
@@ -0,0 +1,28 @@
+# AWS Provider를 Terraform이 다운로드하도록 설정
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = var.environment
+  region  = "ap-northeast-2" # 원하는 리전
+}
+
+# module "component" {
+#   source      = "./component"
+#   environment = var.environment
+#   enable      = var.enable_sandbox #|| var.environment == "prod"
+# }
+#
+# module "discord-bot" {
+#   source             = "./discord-bot/infra"
+#   count              = var.environment == "sandbox" ? 1 : 0
+#   environment        = var.environment
+#   discord_public_key = var.discord_public_key
+#   github_token       = var.github_token
+# }
