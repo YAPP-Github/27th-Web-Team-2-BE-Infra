a6d1e9208d695b5e759b6e93dc552a822428d222
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-12-22 21:10:33 +0900

    4 feat: sandbox ECS 서비스 및 태스크 정의 추가

---
+++ component/sandbox/ecs-ec2/task_definition.tf
@@ -0,0 +1,40 @@
+resource "aws_cloudwatch_log_group" "app" {
+  name              = "/ecs/${var.environment}/app"
+  retention_in_days = var.log_retention_in_days
+}
+
+resource "aws_ecs_task_definition" "app" {
+  family                   = "${var.environment}-app"
+  network_mode             = "bridge" # ALB 없는 단계에서 EC2 Public IP + hostPort 테스트 가능
+  requires_compatibilities = ["EC2"]
+  cpu                      = tostring(var.cpu)
+  memory                   = tostring(var.memory)
+
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn      = aws_iam_role.ecs_task_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "app"
+      image     = var.container_image
+      essential = true
+
+      portMappings = [
+        {
+          containerPort = var.container_port
+          hostPort      = var.container_port
+          protocol      = "tcp"
+        }
+      ]
+
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.app.name
+          awslogs-region        = "ap-northeast-2"
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+    }
+  ])
+}
