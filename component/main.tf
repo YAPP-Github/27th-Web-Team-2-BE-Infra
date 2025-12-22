ab5cf1635f46280c55b70557ccaf125d87120895
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-12-22 21:59:12 +0900

    4 feat: sandbox ECS EC2 모듈 연결 및 실행 환경 구성

--- component/main.tf
+++ component/main.tf
@@ -1,9 +1,21 @@
 module "sandbox" {
-  count = var.environment == "sandbox" ? 1 : 0
+  count  = var.environment == "sandbox" ? 1 : 0
   source = "./sandbox"
+
+  vpc_id             = aws_vpc.main.id
+  public_subnet_ids  = [
+    aws_subnet.public_a.id,
+    aws_subnet.public_c.id
+  ]
 }
 
 module "prod" {
-  count = var.environment == "prod" ? 1 : 0
+  count  = var.environment == "prod" ? 1 : 0
   source = "./prod"
+
+  vpc_id             = aws_vpc.main.id
+  public_subnet_ids  = [
+    aws_subnet.public_a.id,
+    aws_subnet.public_c.id
+  ]
 }
