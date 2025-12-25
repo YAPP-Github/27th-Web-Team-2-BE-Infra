a3dbcb200e25edc782addafc13fbbdf2fe51c634
comforest <comforest@naver.com>
2025-12-25 17:50:00 +0900

    36 fix: 팀 이름 반영

--- component/cloudwatch.tf
+++ component/cloudwatch.tf
@@ -1,4 +1,4 @@
-resource "aws_cloudwatch_log_group" "woossu_transaction_log" {
+resource "aws_cloudwatch_log_group" "nomoney_transaction_log" {
   name              = format("/%s/nomoney/server/transaction_log", var.environment)
   retention_in_days = 14 # 로그 보존 기간 (필요에 따라 변경 가능)
 
@@ -7,7 +7,7 @@ resource "aws_cloudwatch_log_group" "woossu_transaction_log" {
   }
 }
 
-resource "aws_cloudwatch_log_group" "woossu_console_log" {
+resource "aws_cloudwatch_log_group" "nomoney_console_log" {
   name              = format("/%s/nomoney/server/console_log", var.environment)
   retention_in_days = 14 # 로그 보존 기간 (필요에 따라 변경 가능)
 
