1105b7751ed3a009ffd15adaba696f85416391da
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-12-30 01:06:43 +0900

    41 refactor: startPeriod 60 -> 180으로 연장

--- component/ecs-ec2/task_definition.tf
+++ component/ecs-ec2/task_definition.tf
@@ -32,7 +32,7 @@ resource "aws_ecs_task_definition" "app" {
         interval    = 30
         timeout     = 5
         retries     = 3
-        startPeriod = 60
+        startPeriod = 180
       }
 
       logConfiguration = {
