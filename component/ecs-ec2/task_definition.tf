e5f0ae0577786c4623676d1e1aa23800b425f98f
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-12-29 20:31:33 +0900

    41 feat: ECS Health Check 관련된 설정 - Health Check Path - Health Check 검사 유예시간

--- component/ecs-ec2/task_definition.tf
+++ component/ecs-ec2/task_definition.tf
@@ -27,6 +27,14 @@ resource "aws_ecs_task_definition" "app" {
         }
       ]
 
+      healthCheck = {
+        command     = ["CMD-SHELL", "curl -f http://localhost:${var.container_port}/ping || exit 1"]
+        interval    = 30
+        timeout     = 5
+        retries     = 3
+        startPeriod = 60
+      }
+
       logConfiguration = {
         logDriver = "awslogs"
         options = {
