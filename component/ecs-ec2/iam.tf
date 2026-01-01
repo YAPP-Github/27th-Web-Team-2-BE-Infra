841cf2b7b61725f36df4b7b3e96b9f683cda5e60
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2026-01-02 02:37:09 +0900

    38 fix: Parameter Store 권한 Task Role 로 변경

--- component/ecs-ec2/iam.tf
+++ component/ecs-ec2/iam.tf
@@ -51,8 +51,8 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_attach" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_ssm_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
+resource "aws_iam_role_policy_attachment" "ecs_task_role_ssm_policy" {
+  role       = aws_iam_role.ecs_task_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
 }
 
