9ecfc684d383236b0ba4c2278f1f2994004fca4f
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2026-01-02 15:02:59 +0900

    54 refactor: ECR 인프라 리소스 네이밍 정비 및 책임 경계 반영

--- component/ecr/lifecycle.tf
+++ component/ecr/lifecycle.tf
@@ -1,11 +1,11 @@
-resource "aws_ecr_lifecycle_policy" "app" {
-  repository = aws_ecr_repository.app.name
+resource "aws_ecr_lifecycle_policy" "shared_app_retention" {
+  repository = aws_ecr_repository.shared_app.name
 
   policy = jsonencode({
     rules = [
       {
         rulePriority = 1
-        description  = "Keep last 10 images"
+        description  = "Keep last 3 images"
         selection = {
           tagStatus   = "any"
           countType   = "imageCountMoreThan"
