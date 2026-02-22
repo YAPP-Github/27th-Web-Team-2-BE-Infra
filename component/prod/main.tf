91d0f5fac6cdc4fe1ab6bd0e73dbdeb91792ce2a
comforest <comforest@naver.com>
2026-02-22 22:31:54 +0900

    71 fix: sandbox 도메인 이름 변경

--- component/prod/main.tf
+++ component/prod/main.tf
@@ -1,9 +1,9 @@
 locals {
   enable_custom_domain = contains(["prod", "sandbox"], var.environment)
-  primary_zone_name    = var.environment == "prod" ? "moit.kr" : "dev.moit.kr"
-  secondary_zone_name  = var.environment == "prod" ? "weddin.kr" : "dev.weddin.kr"
-  alb_primary_domain   = var.environment == "prod" ? "api.moit.kr" : "dev.moit.kr"
-  alb_secondary_domain = var.environment == "prod" ? "api.weddin.kr" : "dev.weddin.kr"
+  primary_zone_name    = var.environment == "prod" ? "moit.kr" : "sandbox-api.moit.kr"
+  secondary_zone_name  = var.environment == "prod" ? "weddin.kr" : "sandbox-api.weddin.kr"
+  alb_primary_domain   = var.environment == "prod" ? "api.moit.kr" : "sandbox-api.moit.kr"
+  alb_secondary_domain = var.environment == "prod" ? "api.weddin.kr" : "sandbox-api.weddin.kr"
 
   cert_validation_zone_by_domain = local.enable_custom_domain ? {
     (local.alb_primary_domain)   = aws_route53_zone.primary[0].zone_id
